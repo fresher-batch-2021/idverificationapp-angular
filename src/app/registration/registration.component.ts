c9d2be2b36ea3b3aa3f9130f4b25c1f0f3cf3bfa
Ganeshkumar S <Gane2846@global.chainsys.com>
2021-08-10 17:59:35 +0530

    upload

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import axios from 'axios';
 
 @Component({
   selector: 'app-registration',
@@ -15,6 +16,7 @@ export class RegistrationComponent implements OnInit {
   date_of_birth:string="" ;
   emailid:string="";
   username:string="";
+  number:string="";
   create_password:string="";
   confirmpassword:string="";
 
@@ -27,6 +29,9 @@ export class RegistrationComponent implements OnInit {
     if(this.emailid == "" ){
       alert("invalid email");
     }
+    if(this.number ==""){
+      alert("Enter_phone_number")
+    }
     if(this.username == "" ){
       alert("invalid username");
     }
@@ -35,11 +40,24 @@ export class RegistrationComponent implements OnInit {
     }
     if(this.confirmpassword == "" ){
       alert("No_match_password");
-    }
-    else{
-      alert("successfull");
-      window.location.href="loginpage"
-    }
+    } else{
+
+      const url = "https://product-mock-api.herokuapp.com/identityapp/api/v1/auth/register"
+      const registerData = {
+          name: this.yourname,
+          dob: this.date_of_birth,
+          email: this.emailid,
+          username: this.username,
+          mobileNo: this.number,
+          password: this.create_password  
+        }
+      
+      axios.post(url, registerData).then(res => {
+          console.log(registerData);
+          alert("successfull")
+          window.location.href = "loginpage";
+        }).catch(err => alert("error "))
+      }
   }
 
 
